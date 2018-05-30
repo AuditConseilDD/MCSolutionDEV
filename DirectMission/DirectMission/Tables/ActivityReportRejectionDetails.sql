1035b2962ec55f218e4f5064c139f812ecf7a3e5
mdaniou <md@mxbzh.com>
2018-05-30 21:58:37 +0200

    removing the Activities tables

--- DirectMission/DirectMission/Tables/ActivityReportRejectionDetails.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ActivityReportRejectionDetails] (
-    [ActivityReportRejectionDetailsID] INT            IDENTITY (1, 1) NOT NULL,
-    [Details]                          NVARCHAR (512) NOT NULL,
-    CONSTRAINT [PK_ActivityReportRejectionDetails_ARejD_ActivityReportRejectionDetailsID] PRIMARY KEY CLUSTERED ([ActivityReportRejectionDetailsID] ASC)
-);
-
--- DirectMission/DirectMission/Tables/ActivityTypes.sql
+++
@@ -1,6 +0,0 @@
-﻿CREATE TABLE [dbo].[ActivityTypes] (
-    [ActivityTypesID] INT            NOT NULL,
-    [ActivityName]    NVARCHAR (100) NOT NULL,
-    CONSTRAINT [PK_ActivityTypes_ATyp_ActivityTypesID] PRIMARY KEY CLUSTERED ([ActivityTypesID] ASC)
-);
-
